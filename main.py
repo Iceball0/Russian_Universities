b74db9e166c2419f578abe185c4f9d5fd1931316
Владислав Хабибулин <vlad.kh568@gmail.com>
2021-04-24 15:52:32 +0300

    edit procfile

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ from commands import create_tables
 # инициализация Flask и login manager
 app = Flask(__name__)
 api = Api(app)
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SECRET_KEY'] = 'russian_universities_secret_key_1396'
+app.secret_key = 'russian_universities_secret_key_1396'
 app.config.from_pyfile('settings.py')
 app.cli.add_command(create_tables)
 SQLAlchemy().init_app(app)
