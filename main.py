0a283b6164daee4fc39894303d160abc39586b53
Vlad <39436224+Iceball0@users.noreply.github.com>
2022-09-14 19:53:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def main():
 
     # запуск приложения
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 # загрузка аккаунта администратора
